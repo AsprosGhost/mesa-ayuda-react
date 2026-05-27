539f09cbfd776fab841c3339b903fecc03d1d8b5
AsprosGhost <correo@gmail.com>
2026-05-27 12:26:42 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom"
+
 import Login from "./components/Login"
+import Home from "./pages/Home"
 
 function App() {
+
   return (
-    <div>
-      <Login />
-    </div>
+
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path="/" element={<Login />} />
+
+        <Route path="/home" element={<Home />} />
+
+      </Routes>
+
+    </BrowserRouter>
+
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="container mt-5">
+      <h1>Bienvenido al Home 😎</h1>
+    </div>
+  )
+}
+
+export default Home
