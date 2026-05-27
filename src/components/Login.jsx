01b943536c20a55572a652e28f38e144794c8708
AsprosGhost <correo@gmail.com>
2026-05-27 11:26:00 -0600

    primer componente login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,28 @@
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="container mt-5">
+      <div
+        className="card shadow p-4 mx-auto bg-dark text-white"
+        style={{ maxWidth: "400px" }}
+      >
+        <h1 className="text-center mb-4">Login</h1>
+
+        <input
+          type="text"
+          className="form-control mb-3"
+          placeholder="Usuario"
+        />
+
+        <input
+          type="password"
+          className="form-control mb-3"
+          placeholder="Contraseña"
+        />
+
+        <button className="btn btn-primary w-100">
+          Iniciar Sesión
+        </button>
+      </div>
     </div>
   )
 }
